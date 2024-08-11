2a48a34121d27e55862050871ea858f92c059648
Kaihy <51046663+Kaihy@users.noreply.github.com>
2024-08-11 23:35:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
     user: 'postgres',       // Replace with your PostgreSQL username
     host: 'localhost',      // Replace with your PostgreSQL host
     database: 'Tennis_App', // Replace with your PostgreSQL database name
-    password: '12345',      // Replace with your PostgreSQL password
+    password: '',      // Replace with your PostgreSQL password
     port: 5432,             // Default PostgreSQL port
 });
 
